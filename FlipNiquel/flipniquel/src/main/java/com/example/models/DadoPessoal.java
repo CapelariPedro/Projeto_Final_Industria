1623d0ad1400bf03309f355386394431309e334a
juumarionn <juliamarionmendess@gmail.com>
2025-05-13 00:05:17 -0300

    Add files via upload

--- FlipNiquel/flipniquel/src/main/java/com/example/models/DadoPessoal.java
+++ FlipNiquel/flipniquel/src/main/java/com/example/models/DadoPessoal.java
@@ -1,12 +1,17 @@
 package com.example.models;
 
+import java.time.LocalDate;
+
+import javafx.scene.control.Alert;
+
 public class DadoPessoal {
     private int id;
     private String nome_completo;
-    private String data_nascimento;
+    private LocalDate data_nascimento;
     private String sexo;
     private String estado_civil;
     private String conjuge;
+    private LocalDate data_conjugue;
     private String dependentes;
     private String nacionalidade;
     private String naturalidade;
@@ -19,7 +24,79 @@ public class DadoPessoal {
     private String tipo_sanguineo;
     private String contato_emergencia;
 
-    public DadoPessoal(int id, String nome_completo, String data_nascimento, String sexo,  String estado_civil, String conjuge, String dependentes, String nacionalidade, String naturalidade,
+
+      
+    // Método para validar o CPF
+    public boolean validarCPF(String cpf) {
+        // Remove qualquer formatação do CPF
+        cpf = cpf.replaceAll("[^0-9]", "");
+        
+        // Verifica se o CPF tem 11 dígitos
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        // Verifica se o CPF é uma sequência repetida (exemplo: 111.111.111.11)
+        if (cpf.equals("00000000000") || cpf.equals("11111111111") || cpf.equals("22222222222") ||
+            cpf.equals("33333333333") || cpf.equals("44444444444") || cpf.equals("55555555555") ||
+            cpf.equals("66666666666") || cpf.equals("77777777777") || cpf.equals("88888888888") ||
+            cpf.equals("99999999999")) {
+            return false;
+        }
+
+        // Calcula os dígitos verificadores
+        int soma = 0, peso = 10;
+        for (int i = 0; i < 9; i++) {
+            soma += Integer.parseInt(String.valueOf(cpf.charAt(i))) * peso--;
+        }
+        int digito1 = 11 - (soma % 11);
+        if (digito1 >= 10) {
+            digito1 = 0;
+        }
+
+        soma = 0;
+        peso = 11;
+        for (int i = 0; i < 10; i++) {
+            soma += Integer.parseInt(String.valueOf(cpf.charAt(i))) * peso--;
+        }
+        int digito2 = 11 - (soma % 11);
+        if (digito2 >= 10) {
+            digito2 = 0;
+        }
+
+        // Verifica se os dois dígitos verificadores estão corretos
+        return cpf.charAt(9) == (char) (digito1 + '0') && cpf.charAt(10) == (char) (digito2 + '0');
+    }
+
+    // Método para salvar os dados
+    public void salvarDados() {
+        String cpf = this.cpf; // Acessa o valor do campo cpf da classe
+
+
+        // Valida o CPF antes de prosseguir com o salvamento
+        if (!validarCPF(cpf)) {
+            // Exibe uma mensagem de erro, por exemplo
+            exibirMensagemErro("CPF inválido");
+            return;  // Não prossegue se o CPF for inválido
+        }
+
+        // Prosseguir com o processo de salvar os dados
+        // Lógica de salvar o CPF e outros dados
+    }
+
+    // Método para exibir uma mensagem de erro
+    private void exibirMensagemErro(String mensagem) {
+        // Exibindo erro, pode ser um alerta JavaFX
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erro");
+        alert.setHeaderText(null);
+        alert.setContentText(mensagem);
+        alert.showAndWait();
+    }
+}
+
+
+    public DadoPessoal(int id, String nome_completo, LocalDate data_nascimento, String sexo,  String estado_civil, String conjuge, LocalDate data_conjugue, String dependentes, String nacionalidade, String naturalidade,
     String cpf, String rg, String endereco, String telefone, String email, String filiacao, String tipo_sanguineo, String contato_emergencia) {
         this.id = id;
         this.nome_completo = nome_completo;
@@ -27,6 +104,7 @@ public class DadoPessoal {
         this.sexo = sexo;
         this.estado_civil = estado_civil;
         this.conjuge = conjuge;
+        this.data_conjugue = data_conjugue;
         this.dependentes = dependentes;
         this.nacionalidade = nacionalidade;
         this.naturalidade = naturalidade;
@@ -54,10 +132,10 @@ public void setNome_completo(String nome_completo){
     this.nome_completo = nome_completo;
 }
 
-public String getData_nascimento() {
+public LocalDate getData_nascimento() {
     return data_nascimento;
 }
-public void setData_nascimento(String data_nascimento){
+public void setData_nascimento(LocalDate data_nascimento){
     this.data_nascimento = data_nascimento;
 }
 
@@ -80,6 +158,13 @@ public String getConjuge() {
 }
 public void setConjuge(String conjuge){
     this.conjuge = conjuge;
+}
+
+public LocalDate getdata_conjuge() {
+    return data_conjugue;
+}
+public void setdata_conjuge(LocalDate data_conjuge){
+    this.data_conjugue = data_conjuge;
 }
 
 public String getDependentes(){
